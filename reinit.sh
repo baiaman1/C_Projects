2ad109915797ef352aea029f16c401278d40bdaf
Fenix Necromancer <fenixnec@ga-n6.msk.21-school.ru>
2023-01-05 17:50:23 +0300

    file

---
+++ reinit.sh
@@ -0,0 +1,7 @@
+git checkout master   
+git branch main master -f    
+git checkout main  
+git push origin main -f 
+
+
+ <a href="https://github.com/baiaman1/github-readme-stats"><img align="center" src="https://github-readme-stats.vercel.app/api?username=baiaman1&show_icons=true&include_all_commits=true&theme=buefy&hide_border=true" alt="Anurag's github stats" /></a> | <a href="https://github.com/baiaman1/github-readme-stats"><img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=baiaman1&layout=compact&theme=buefy&hide_border=true" /></a>
